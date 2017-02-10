e5246084de178600cf1dd39e450427202ca938a6
jloemba <jorisloemba@gmail.com>
2017-02-10 18:49:32 +0100

    Landing Page v0.4

--- mail/index.php
+++ mail/index.php
@@ -1,5 +1,4 @@
 <?php
-if(isset($_POST['email'])){
 
 	/* Récupération des valeurs des champs du formulaire */
 	    $nom = $_POST['nom'];
@@ -8,23 +7,15 @@ if(isset($_POST['email'])){
       $entse = $_POST['entse'];
       $message = $_POST['message'];
 	    $sujet = "Co-P'r - Demande de contact" ;
-	/* Destinataire (ton adresse e-mail) */
 
+	/* Destinataire (ton adresse e-mail) */
 	    $to = 'contact@co-pr.net';
 
 	/* En-têtes de l'e-mail */
     $headers = 'From: '.$nom.' '.$prenom.'('.$entse.')'.' <'.$mail.'>'."\r\n\r\n";
 
-
 	/* Envoi de l'e-mail */
       mail($to, $sujet, $message, $headers);
-
       header('Location: ../index.html');
-
-}
-else{
-	header('Location: ../index.html');
-}
-
 
 ?>
